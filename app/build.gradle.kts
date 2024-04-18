262c830ed1705229c79a37a2c713177e70a3c30c
Raul Piqueras Melero <rpm115@alu.ua.es>
2024-04-18 20:52:13 +0200

    Setup Inicial Del Proyecto

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.mastermovilesua.runtruckerraul"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.mastermovilesua.runtruckerraul"
