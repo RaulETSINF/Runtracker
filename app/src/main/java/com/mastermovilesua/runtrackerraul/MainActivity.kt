39e1a53f642303e8fc159067d6bd39b200ad7187
Raul Piqueras Melero <rpm115@alu.ua.es>
2024-04-20 15:13:22 +0200

    Bottom Navigation, Fragments, Navigation Graph
    Realizamos la estructura inicial de la aplicación, una bottom bar con 4 fragmentos principales para cada una de las funcionalidades de la app, implementado mediante la navegación con fragmentos y grafo de navegación de Android.

--- app/src/main/java/com/mastermovilesua/runtrackerraul/MainActivity.kt
+++ app/src/main/java/com/mastermovilesua/runtrackerraul/MainActivity.kt
@@ -2,11 +2,24 @@ package com.mastermovilesua.runtrackerraul
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.mastermovilesua.runtrackerraul.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.fragmentContainerView)
+        binding.bottomNav.setupWithNavController(navController)
     }
 
 
---
+++ app/src/main/java/com/mastermovilesua/runtrackerraul/ui/RecordFragment.kt
@@ -0,0 +1,19 @@
+package com.mastermovilesua.runtrackerraul.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mastermovilesua.runtrackerraul.R
+
+class RecordFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_record, container, false)
+    }
+
+}
