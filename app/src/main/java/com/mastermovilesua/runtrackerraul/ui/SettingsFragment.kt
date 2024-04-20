b92166387e93e474aa3e4394c6dd378bb4e320e5
Raul Piqueras Melero <rpm115@alu.ua.es>
2024-04-20 19:31:09 +0200

    Update Settings Fragment
    Empezamos a definir las shared Prefrences, he cambiado el Settings Fragment de un Fragmento Normal a un Settings fragment para que sea mucho mas comodo trabajar con las preferences.

--- app/src/main/java/com/mastermovilesua/runtrackerraul/ui/SettingsFragment.kt
+++ app/src/main/java/com/mastermovilesua/runtrackerraul/ui/SettingsFragment.kt
@@ -1,19 +1,12 @@
 package com.mastermovilesua.runtrackerraul.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.preference.PreferenceFragmentCompat
 import com.mastermovilesua.runtrackerraul.R
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
     }
-
 }
