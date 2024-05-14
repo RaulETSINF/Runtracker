5c16f49dcabcb642a8b15ab3b0dc6ac14ae96fd0
Raul Piqueras Melero <rpm115@alu.ua.es>
2024-05-14 21:30:41 +0200

    Gestion De las Notificaciones

--- app/src/main/java/com/mastermovilesua/runtrackerraul/MainActivity.kt
+++ app/src/main/java/com/mastermovilesua/runtrackerraul/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.mastermovilesua.runtrackerraul
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
 import androidx.navigation.Navigation
@@ -17,13 +23,54 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
+        val requestPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            val isLocationPermissionGranted = permissions[Manifest.permission.ACCESS_FINE_LOCATION] ?: false
+            val isActivityRecognitionPermissionGranted = permissions[Manifest.permission.ACTIVITY_RECOGNITION] ?: false
+            val isNotificationPermissionGranted = permissions[Manifest.permission.POST_NOTIFICATIONS] ?: false
 
-        val navController = findNavController(R.id.fragmentContainerView)
-        binding.bottomNav.setupWithNavController(navController)
+            if (isLocationPermissionGranted && isActivityRecognitionPermissionGranted && isNotificationPermissionGranted) {
+                binding = ActivityMainBinding.inflate(layoutInflater)
+                setContentView(binding.root)
+
+                AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
+
+                val navController = findNavController(R.id.fragmentContainerView)
+                binding.bottomNav.setupWithNavController(navController)
+            } else {
+                showPermissionSettingsDialog()
+            }
+        }
+
+        requestPermissionLauncher.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACTIVITY_RECOGNITION,
+                Manifest.permission.POST_NOTIFICATIONS
+            )
+        )
+
+    }
+
+
+    private fun showPermissionSettingsDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Permisos requeridos")
+            .setMessage("La aplicación requiere permisos de ubicación para funcionar correctamente. Por favor, habilite los permisos en la configuración de la aplicación.")
+            .setPositiveButton("Configuración") { dialog, _ ->
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                val uri = Uri.fromParts("package", this.packageName, null)
+                intent.data = uri
+                startActivity(intent)
+                dialog.dismiss()
+            }
+            .setNegativeButton("Cancelar") { dialog, _ ->
+                dialog.dismiss()
+            }
+        val dialog = builder.create()
+        dialog.show()
     }
 
 }
